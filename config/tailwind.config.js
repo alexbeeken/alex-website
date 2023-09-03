772f117e82e3244de004df3f34f7ca25c0b81f02
Alex Beeken <aeb242@gmail.com>
2023-09-03 13:03:51 -0700

    add headshot circle and some color customization

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      '1': '#312F2F',
+      '2': '#84DCCF',
+      '3': '#A6D9F7',
+      '4': '#BCCCE0',
+      '5': '#BF98A0',
+    },
+  },
   },
   plugins: [
     require('@tailwindcss/forms'),
